4b5586f1e82dfbda960d4f3202b8c3e93fbcd227
Moorthy RS <rsmoorthy@gmail.com>
2018-06-21 12:47:17 +0530

    update notifications code

--- server/services/notifications.js
+++ server/services/notifications.js
@@ -6,42 +6,56 @@ var ejs = require('ejs')
 var moment = require('moment')
 
 const notificationSend = async (templName, row, userId) => {
-  let err, msg
-
-  msg = await Templates.findOne({ name: templName, type: 'Notification' })
-    .exec()
-    .catch(e => (err = e.message))
-  if (!msg) return ['Unable to send notification: ' + err]
+  let err, msg, type
+  msg = null
 
   switch (templName) {
     case 'WelcomePassenger':
     case 'NotifyReceiver':
+      type = 'pickup'
       row.pickupDate = moment(row.pickupDate).format('Do MMM YYYY HH:mm')
       break
 
     case 'PassengerTripCompleted':
+      type = 'pickup'
       row.pickupDate = moment(row.pickupDate).format('Do MMM YYYY HH:mm')
       row.completedDate = moment(row.completedDate).format('Do MMM YYYY HH:mm')
       break
 
     case 'ReceiverTripCompleted':
+      type = 'pickup'
       row.pickupDate = moment(row.pickupDate).format('Do MMM YYYY HH:mm')
       row.completedDate = moment(row.completedDate).format('Do MMM YYYY HH:mm')
+      break
+
+    case 'Logout':
+      type = 'logout'
+      msg = { template: 'Forced Logout' }
       break
 
     default:
       return ['Invalid Notification Template name']
   }
 
+  if (!msg) {
+    msg = await Templates.findOne({ name: templName, type: 'Notification' })
+      .exec()
+      .catch(e => (err = e.message))
+  }
+  if (!msg) return ['Unable to send notification: ' + err]
+  console.log('step 1', msg)
+
   let urow = await Users.findById(userId).exec()
   if (!urow || (urow && !urow.expoToken)) return [null, '']
 
+  console.log('step 2', urow)
   let [err2, resp] = await sendNotification({
     token: urow.expoToken,
     body: ejs.render(msg.template, row),
-    data: { body: ejs.render(msg.template, row), id: row._id }
+    data: { body: ejs.render(msg.template, row), type: type, id: row._id }
   })
 
+  console.log('step 3', err2, resp)
   return [err2, resp]
 }
 
